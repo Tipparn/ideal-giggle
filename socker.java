2a6ab7a686a96cc87f576aaa9c8374ac18b108f9
Fredrik <fredrikkortetjarvi@protonmail.com>
2019-05-02 21:11:50 +0200

    this is only more testing so dont worry.

--- socker.java
+++ socker.java
@@ -2,4 +2,7 @@ class socker{
 	public static void main(String[] args){
 		System.out.print("here is a change");
 	}
+	public void floris(String Allt) {
+		System.out.print("here we have everything for you")
+	}
 }
