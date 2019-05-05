cab06885daadd335fdc73602e86643864cec39ca
Tipparn <43550846+Tipparn@users.noreply.github.com>
2019-05-05 09:52:46 +0200

    this is only testing like always

--- test.java
+++ test.java
@@ -1,8 +1,6 @@
-class test {
-    public static void main(String[] args){
-        System.out.print("hello world!");
-    }
-public static void Stop(){
-    System.out.print("godag");
-}
+class test{
+	public static void main( String[] args){
+		System.out.println("hello world!");
+	}
+
 }
